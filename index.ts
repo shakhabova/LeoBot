ea3b8b964fe76c864ba6ef99742c20c05c1a8246
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-02-27 09:54:06 +0300

    crone fixed

--- index.ts
+++ index.ts
@@ -186,7 +186,7 @@ const birthdayCrone3 = new CronJob(
 );
 
 const bd_Ruslan = new CronJob(
-  '00 09 27 02 *', // cronTime
+  '00 10 27 02 *', // cronTime
   function () {
     bot.api.sendMessage(
       chatId,
