28a92e04e84edcf7e656f253e37fdbb87f381627
Алина Шахабова <alina.shakhabova@yandex.ru>
2025-05-12 13:57:00 +0300

    crone

--- index.ts
+++ index.ts
@@ -199,7 +199,7 @@ const bd_Ruslan = new CronJob(
 );
 
 const bd_AdamSul = new CronJob(
-  '56 13 12 05 *', // cronTime
+  '59 13 12 05 *', // cronTime
   function () {
     bot.api.sendMessage(
       chatId,
